f159fb2eb495dbe54149f2c064baeaab4f2fc4b8
Egor Polzikov <48046466+MTUCI-Egor@users.noreply.github.com>
2019-04-22 11:54:30 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,52 @@
-from flask import Flask
+from flask import Flask, render_template,url_for,flash,redirect
+from forms import RegistrationForm, LoginForm
+app = Flask(__name__)
 
-app =Flask(__name__)
+app.config['SECRET_KEY'] = '6393fd014b7a0e696895d5ee50fb55ec'
 
-@app.route('/')
-def main():
-    return 'hy'
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1',port=5000)
+posts = [
+    {
+        'author':'Egor',
+        'title':'Blog Post 1',
+        'content':'I dont know',
+        'date_posted':'March 8,2019'
+    },
+    {
+        'author': 'SlovЭk',
+        'title': 'Blog Post 2',
+        'content': 'I dont know',
+        'date_posted': 'March 21,2019'
+    }
+]
 
+@app.route("/")
+@app.route("/home")
+def home():
+    return  render_template('home.html',posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html',title='About')
+
+@app.route("/register",methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
+        return redirect (url_for('home'))
+    return render_template('register.html',title = 'Register', form=form)
+
+@app.route("/login",methods=['GET','POST'])
+def Login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!','success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Pease check your email and password', 'danger')
+    return render_template('login.html',title = 'Login', form=form)
+
+if __name__ == "__main__":
+    app.run(debug=True,host='127.0.0.1', port=5000)
